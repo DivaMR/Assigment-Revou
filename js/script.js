078ede406a1387f5ebaa3b54f75c2463d0a76383
= <=>
2024-06-06 16:30:09 +0700

    Update terbaru

--- js/script.js
+++ js/script.js
@@ -4,3 +4,35 @@ function replaceName () {
 }
 
 replaceName();
+
+let namaInput = document.getElementById("nama")
+
+// form validation
+function formvalidation() {
+    let namaInput = document.getElementById('nama').value;
+    console.log(namaInput);
+}
+
+
+// slide banner
+let indexSlide = 1;
+showBanner(1);
+
+function nextSlide (n) {
+    showBanner(indexSlide += n); 
+}
+
+function showBanner(index) {
+    let listImage = document.getElementsByClassName('banner-img');
+    if (index > listImage.length) indexSlide = 1;
+    
+    let index = 0;
+    while (index < listImage.length) {
+        listImage.length[index].style.display = 'none';
+        index++;
+    }
+
+    listImage.length[indexSlide - 1].style.display = 'block';
+}
+
+
